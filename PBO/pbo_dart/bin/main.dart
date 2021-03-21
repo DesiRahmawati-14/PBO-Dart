181dcdee8d1d7e5711723c725872c26533d77614
Desi Rahmawati <rahmawatidesi730@gmail.com>
2021-03-21 10:10:37 +0700

    main dart commit

---
+++ PBO/pbo_dart/bin/main.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:pbo_dart/drink_ability_mixin.dart';
+import 'package:pbo_dart/flying_monster.dart';
+import 'package:pbo_dart/hero.dart';
+import 'package:pbo_dart/knight.dart';
+import 'package:pbo_dart/monster.dart';
+import 'package:pbo_dart/monster_kecoa.dart';
+import 'package:pbo_dart/monster_ubur_ubur.dart';
+import 'package:pbo_dart/monster_ucoa.dart';
+
+main(List<String> args) {
+  // Hero h = Hero();
+  // Monster m = Monster();
+  // MonsterUburUbur u= MonsterUburUbur();
+  List<Monster> monster = [];
+
+  // monster.add(MonsterUburUbur());
+  // monster.add(MonsterKecoa());
+  // monster.add(MonsterUcoa());
+  
+  
+
+ 
+  // for (Monster m in monster){
+  //   if(m is DrinkAbilityMixin){
+  //     print((m as DrinkAbilityMixin).drink());
+  //   }
+  
+  // }
+  // h.healthPoint = -10;
+  // u.healthPoint = 3 ;
+
+  // print("hero HP: " + h.healthPoint.toString());
+  // print("monster HP:" + m.healthPoint.toString());
+  // print(h.killAmonster());
+  // print(u.eatHuman());
+  // print(u.swim());
+
+}
