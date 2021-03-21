4e789d16fa6dbd4e4ce706077d5445d91499b364
Desi Rahmawati <rahmawatidesi730@gmail.com>
2021-03-21 09:28:48 +0700

    class objek commit

---
+++ PBO/pbo_dart/bin/classobjek.dart
@@ -0,0 +1,23 @@
+import 'package:pbo_dart/pbo_dart.dart' as dasar_dart;
+import 'dart:io';
+import 'package:pbo_dart/persegi_panjang.dart';
+
+main(List<String> args) {
+    PersegiPanjang kotak1, kotak2   ;
+    double luasKotak1;
+
+    kotak1 = new PersegiPanjang();
+    kotak1.setPanjang(-2);
+    kotak1.lebar = 3;
+
+    kotak2 = PersegiPanjang();
+    kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
+    kotak2.lebar = double.tryParse(stdin.readLineSync());
+
+    luasKotak1 = kotak1.hitungluas();
+
+
+    print(luasKotak1 + kotak2.hitungluas());
+
+    print(kotak1.getpanjang());
+}
